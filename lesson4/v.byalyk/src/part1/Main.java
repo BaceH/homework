f968c0eabb459bfd4afc3932e46947b71f678452
Your Name <you@example.com>
2016-10-25 21:31:24 +0300

    error: cannot find symbol

---
+++ lesson4/v.byalyk/src/part1/Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String ... args) {
+    MoreArrays mArrays = new MoreArrays();
+//    int[][] mA1 = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+//   int[][] mA2 = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+//    System.out.println(mArrays.areEqual(mA1, mA2) );
+  }
+}
---
+++ lesson4/v.byalyk/src/part1/MoreArrays.java
@@ -0,0 +1,19 @@
+class MoreArrays {
+    private boolean state;
+    public static boolean areEqual(int[][] arr1, int[][] arr2) {
+        return true;
+    }
+ /*   public static int[] linearize(int[][] arr) {
+        return { 1 , 2 , 3 };
+    }
+    public static int[][] createSpiral(int n, int m) {
+        return { { 1 , 2 , 3 } , { 1 , 2 , 3 } };
+    }
+    public static int[][] transpose(int[][] arr) {
+        return { { 1 , 2 , 3 } , { 1 , 2 , 3 } };
+    }
+    public static void print(int[][] arr) {
+        System.out.println("true");
+    }
+*/
+}
